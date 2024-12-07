7f43971f4185a1b45a27689839a2c3c9141e9a1f
khanhdepzai206 <nguyenkhanh200603@gmail.com>
2024-12-08 04:57:00 +0700

    first commit

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from.models import *
+
+# Create your views here.
+def home(request) :
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request,'home.html',context)
+def cart(request) :
+        if request.user.is_authenticated :
+            customer = request.user.customer
+            order,created =Order.objects.get_or_create(customer = customer ,complete=False)
+            items = order.orderitem_set.all()
+        else :
+            items=[]
+        context ={'items':items,'order':order}
+        return render(request,'cart.html',context)
+def checkout(request) :
+    context={}
+    return render(request ,'checkout.html',context)
+
